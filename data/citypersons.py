51dbaff65d77ee555e46bcff28fecce8444480a5
Narayan Murti <nam27@st-andrews.ac.uk>
2025-02-12 10:59:48 +0000

    Update citypersons.py remove xml import

--- data/citypersons.py
+++ data/citypersons.py
@@ -16,7 +16,6 @@ import torchvision
 from PIL import Image
 from tqdm import tqdm
 from torch.utils.data.dataset import Dataset
-import xml.etree.ElementTree as ET
 
 def load_images_and_anns(im_dir, ann_file):
     r"""
