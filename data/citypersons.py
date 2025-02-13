06ab9bbfda91f465ff140f2f49c116e513c60f52
Narayan Murti <nam27@st-andrews.ac.uk>
2025-02-13 15:58:43 +0000

    Update citypersons.py test entering the bounding boxes differently

--- data/citypersons.py
+++ data/citypersons.py
@@ -75,8 +75,10 @@ def load_images_and_anns(im_dir, ann_file):
             bbox = [
                 int(list(row['x1'])[i]),
                 int(list(row['y1'])[i]),
-                int(list(row['x1'])[i]) + int(list(row['w'])[i]),
-                int(list(row['x1'])[i]) + int(list(row['h'])[i])
+                #int(list(row['x1'])[i]) + int(list(row['w'])[i]),
+                #int(list(row['x1'])[i]) + int(list(row['h'])[i])
+                int(list(row['w'])[i]),
+                int(list(row['h'])[i])
             ]
             det['label'] = list(row['class_label'])[i] # **FOR NOW IM JUST GOING TO LABEL THEM WITH THE NUMBERS**
             det['bbox'] = bbox
